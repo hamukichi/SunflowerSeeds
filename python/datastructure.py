c6d5ada6e0c4713c2e503d9cd3891284f81fa132
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2016-07-15 13:17:13 +0900

    Added datastructure.py
    Includes CumulativeSum.

---
+++ python/datastructure.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import itertools
+
+
+class CumulativeSum(object):
+    """Constructs the cumulative sum of the elements in the given sequence.
+
+    :param sequence: The sequence to be processed.
+    :type sequence: sequence
+    """
+
+    def __init__(self, sequence):
+        self.cumulative_sum = [0]
+        self.cumulative_sum.extend(itertools.accumulate(sequence))
+
+    def partial_sum(self, first, last):
+        """Computes the partial sum of the sequence.
+
+        :param first: The index representing the first element of the
+                      subsequence.
+        :type first: int
+        :param last: The index representing the last element of the
+                     subsequence.
+        :return: The partial sum.
+        """
+        return self.cumulative_sum[last + 1] - self.cumulative_sum[first]
