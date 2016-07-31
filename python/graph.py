6ea102bde82ef159f65248c44eb6d3062bf547e8
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2016-07-31 20:53:47 +0900

    added topological_sort

--- python/graph.py
+++ python/graph.py
@@ -4,6 +4,8 @@
 
 """
 
+import array
+import heapq
 import itertools
 
 
@@ -28,3 +30,41 @@ def warshall_floyd(n, adj_matrix):
         adj_matrix[i][j] = min(adj_matrix[i][j],
                                adj_matrix[i][k] + adj_matrix[k][j])
     return adj_matrix
+
+
+class TopologicalSortError(Exception):
+    pass
+
+
+def topological_sort(num_vs, adj_vs):
+    """ Performs topological sorting of the given DAG.
+
+    This function computes the lexicographically least
+    topological order of the given DAG.
+
+    :param num_vs int: The number of vertices.
+    :param adj_vs list: The adjacency list.
+    :return: The lexicographically least topological order.
+    :rtype: :obj:`array.array`
+    :raises TopologicalSortError: if the given graph is not a DAG
+    """
+    in_deg = array.array("L", (0 for _ in range(num_vs)))
+    for dests in adj_vs:
+        for dest in dests:
+            in_deg[dest] += 1
+    sorted_vs = array.array("L")
+    pq = []
+    for s in range(num_vs):
+        if in_deg[s] == 0:
+            heapq.heappush(pq, s)
+    while pq:
+        u = heapq.heappop(pq)
+        sorted_vs.append(u)
+        for v in adj_vs[u]:
+            in_deg[v] -= 1
+            if in_deg[v] == 0:
+                heapq.heappush(pq, v)
+    if all(x == 0 for x in in_deg):
+        return sorted_vs
+    else:
+        raise TopologicalSortError("The graph has a cycle")
