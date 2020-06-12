500feff2d04455f10b5fd05f9deb90da5e30a5e0
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2020-06-12 21:48:59 +0900

    Added primality_test_trial_division

---
+++ verification/python/number-theory/primality_test_trial_division.test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""Determines whether an input integer is prime, by trial division.
+
+Verification: [ALDS1_1_C](https://onlinejudge.u-aizu.ac.jp/status/users/hamukichi/submissions/1/ALDS1_1_C/judge/4573115/Python3)
+"""
+
+
+def is_prime_trial_division(n):
+    """Determing whether the given integer `n` is prime by trial division.
+
+    """
+
+    assert n > 0
+    if n < 2:
+        return False
+    else:
+        for i in range(2, n):
+            if i * i > n:
+                break
+            elif n % i == 0:
+                return False
+        return True
+
+
+def count_primes(ms):
+    return sum(is_prime_trial_division(m) for m in ms)
+
+
+def main():
+    n = int(input())
+    ms = [int(input()) for _ in range(n)]
+    print(count_primes(ms))
+
+
+if __name__ == '__main__':
+    main()
