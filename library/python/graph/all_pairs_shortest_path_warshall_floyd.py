3161dd50c1f64b355149f57e33e550d7a8470afa
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2020-06-12 23:40:16 +0900

    Corrected typo

--- library/python/graph/all_pairs_shortest_path_warshall_floyd.py
+++ library/python/graph/all_pairs_shortest_path_warshall_floyd.py
@@ -14,11 +14,11 @@ INF = 1000000000000000000
 
 
 def in_place_warshall_floyd(adj_matrix):
-    """Solves the all-pairs shortest path problem using the Warshall-Floyd algorithm.
+    r"""Solves the all-pairs shortest path problem using the Warshall-Floyd algorithm.
 
     The adjacency matrix A of the graph. This object must be
     able to be treated as a $n \times n$ matrix. $A_{i, j}$ represents the
-    cost of the edge $i \rightarrow j$. If $A_{i, j}$ is `INF`` or more,
+    cost of the edge $i \rightarrow j$. If $A_{i, j}$ is `INF` or more,
     there is no edge between the two vertices. For all $x$, $A_{x, x} = 0$.
     Note that this matrix will be modified in place during calculation.
     
